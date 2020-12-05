2df0d7c0968e4a95a47f12a6b4de7a7013121a2f
tripthilohit <tripthi.lohit@gmail.com>
2020-12-05 17:11:37 -0500

    min max sum

---
+++ Hackerrank/minMaxSumArray.js
@@ -0,0 +1,20 @@
+function minMaxSum(arr){
+    var output = []
+    var min = 0 , max = 0
+    
+    arr.sort()
+    
+    for(var i = 0; i < arr.length; i++){
+     if(i >= 1)
+       max += arr[i]
+      if(i < arr.length -1)
+        min += arr[i]
+    }
+    
+    output.push(min)
+    output.push(max)
+    
+    console.log(output.join(' '))
+  }
+  
+  minMaxSum([1,2,3,4,5])
